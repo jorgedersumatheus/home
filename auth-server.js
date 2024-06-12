830846de0da477177ad68eb45c80a37110dddaf3
Jorge Luis Aparecido Matheus <jorge@jorgematheus.mus.br>
2024-06-12 12:43:31 -0300

    Update auth-server.js

--- auth-server.js
+++ auth-server.js
@@ -2,57 +2,121 @@ const express = require('express');
 const request = require('request');
 const querystring = require('querystring');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const client_id = '1cb62fe9ab624385b5756ecf29e3edbf'; // Substitua com seu Client ID do Spotify
+const client_secret = '760dad93480d4cbb971e1ea1fb5f5821'; // Substitua com seu Client Secret do Spotify
+const redirect_uri = 'http://www.jorgematheus.mus.br/BLACKVOX_PlayPremium.html'; // Substitua com sua URI de redirecionamento
+
+const generateRandomString = function(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
 const app = express();
 
-const client_id = '1cb62fe9ab624385b5756ecf29e3edbf'; // Seu Client ID
-const client_secret = '760dad93480d4cbb971e1ea1fb5f5821'; // Seu Client Secret
-const redirect_uri = 'http://www.jorgematheus.mus.br/BLACKVOX_PlayPremium.html'; // Sua URL de redirecionamento
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(cookieParser());
 
-app.use(cors());
+app.get('/login', function(req, res) {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
 
-app.get('/login', (req, res) => {
-    const scope = 'user-read-private user-read-email';
+    const scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
             client_id: client_id,
             scope: scope,
             redirect_uri: redirect_uri,
-            state: 'some-state-of-my-choice'
+            state: state
         }));
 });
 
-app.get('/callback', (req, res) => {
+app.get('/callback', function(req, res) {
     const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        res.clearCookie(stateKey);
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                const access_token = body.access_token,
+                      refresh_token = body.refresh_token;
+
+                const options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                request.get(options, function(error, response, body) {
+                    console.log(body);
+                });
+
+                res.redirect('/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
+        });
+    }
+});
+
+app.get('/refresh_token', function(req, res) {
+    const refresh_token = req.query.refresh_token;
     const authOptions = {
         url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')) },
         form: {
-            code: code,
-            redirect_uri: redirect_uri,
-            grant_type: 'authorization_code'
-        },
-        headers: {
-            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
         },
         json: true
     };
 
-    request.post(authOptions, (error, response, body) => {
+    request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             const access_token = body.access_token;
-            const refresh_token = body.refresh_token;
-
-            const uri = 'http://www.jorgematheus.mus.br/BLACKVOX_PlayPremium.html';
-            res.redirect(uri + '?access_token=' + access_token + '&refresh_token=' + refresh_token);
-        } else {
-            res.redirect('/#' +
-                querystring.stringify({
-                    error: 'invalid_token'
-                }));
+            res.send({
+                'access_token': access_token
+            });
         }
     });
 });
 
-app.listen(8888, () => {
-    console.log('Servidor rodando na porta 8888');
-});
+console.log('Servidor rodando na porta 8888');
+app.listen(8888);
