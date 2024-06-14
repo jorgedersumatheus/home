5fc1ac8f907ad68e1daf91026aa254cf6c98f79a
Jorge Luis Aparecido Matheus <jorge@jorgematheus.mus.br>
2024-06-14 13:44:36 -0300

    Update auth-server.js

--- auth-server.js
+++ auth-server.js
@@ -1,33 +1,31 @@
+javascript
+Copiar código
 const express = require('express');
 const request = require('request');
 const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
-
-const client_id = '1cb62fe9ab624385b5756ecf29e3edbf';
-const client_secret = '760dad93480d4cbb971e1ea1fb5f5821';
-const redirect_uri = 'http://www.jorgematheus.mus.br/BLACKVOX_PlayPremium.html/callback';
-
+const cors = require('cors');
 const app = express();
 
-app.use(cookieParser());
+const clientId = '1cb62fe9ab624385b5756ecf29e3edbf';
+const clientSecret = '760dad93480d4cbb971e1ea1fb5f5821';
+const redirectUri = 'https://jorgedersumatheus.github.io/home/BLACKVOX_PlayPremium.html';
 
-// Rota para realizar o login
-app.get('/login', function(req, res) {
-    const state = generateRandomString(16);
-    const scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative';
+app.use(cors());
+app.use(express.json());
 
+app.get('/login', (req, res) => {
+    const scope = 'user-read-private user-read-email';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
-            client_id: client_id,
+            client_id: clientId,
             scope: scope,
-            redirect_uri: redirect_uri,
-            state: state
+            redirect_uri: redirectUri,
+            state: 'some-state-of-my-choice'
         }));
 });
 
-// Rota para obter o token de acesso após o login
-app.get('/callback', function(req, res) {
+app.get('/callback', (req, res) => {
     const code = req.query.code || null;
     const state = req.query.state || null;
 
@@ -37,57 +35,84 @@ app.get('/callback', function(req, res) {
                 error: 'state_mismatch'
             }));
     } else {
-        res.redirect('/get-access-token?code=' + code);
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirectUri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const accessToken = body.access_token;
+                const refreshToken = body.refresh_token;
+
+                res.redirect('/#' +
+                    querystring.stringify({
+                        access_token: accessToken,
+                        refresh_token: refreshToken
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
+        });
     }
 });
 
-// Rota para obter o token de acesso
-app.get('/get-access-token', function(req, res) {
-    const code = req.query.code || null;
-    const authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        form: {
-            code: code,
-            redirect_uri: redirect_uri,
-            grant_type: 'authorization_code'
-        },
+app.post('/get_artist_info', (req, res) => {
+    const { artist_name } = req.body;
+
+    const options = {
+        url: `https://api.spotify.com/v1/search?q=${artist_name}&type=artist`,
         headers: {
-            'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+            'Authorization': 'Bearer ' + accessToken
         },
         json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.get(options, (error, response, body) => {
         if (!error && response.statusCode === 200) {
-            const access_token = body.access_token;
-            const refresh_token = body.refresh_token;
-
-            res.redirect('/#' +
-                querystring.stringify({
-                    access_token: access_token,
-                    refresh_token: refresh_token
-                }));
+            const artist = body.artists.items[0];
+            res.json(artist);
         } else {
-            res.redirect('/#' +
-                querystring.stringify({
-                    error: 'invalid_token'
-                }));
+            res.status(response.statusCode).json(body);
         }
     });
 });
 
-app.listen(8888, () => {
-    console.log('Servidor rodando na porta 8888');
+app.post('/get_spotify_playlists', (req, res) => {
+    const { spotify_user_id } = req.body;
+
+    const options = {
+        url: `https://api.spotify.com/v1/users/${spotify_user_id}/playlists`,
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        json: true
+    };
+
+    request.get(options, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.status(response.statusCode).json(body);
+        }
+    });
 });
 
-function generateRandomString(length) {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
 
 
 
