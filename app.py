e297f35900af1b7410b2f800eca4f96e0a405880
Jorge Luis Aparecido Matheus <jorge@jorgematheus.mus.br>
2024-07-08 02:51:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+app.config['DEBUG'] = True  # Habilitar o modo de depuração
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/register', methods=['POST'])
 def register():
-    username = request.form['username']
-    email = request.form['email']
-    whatsapp = request.form['whatsapp']
-    mensagem = request.form['mensagem']
+    try:
+        username = request.form['username']
+        email = request.form['email']
+        whatsapp = request.form['whatsapp']
+        mensagem = request.form['mensagem']
 
-    # Envia e-mail para você
-    admin_msg = Message('Novo Registro de Usuário',
-                        sender=os.environ.get('MAIL_USERNAME'),
-                        recipients=[os.environ.get('MAIL_USERNAME')])
-    admin_msg.body = f'Username: {username}\nEmail: {email}\nWhatsapp: {whatsapp}\nMensagem: {mensagem}'
+        # Configuração do email
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587
+        smtp_user = 'www.jorgematheus.mus.br@gmail.com'
+        smtp_password = 'dqua lyrj atat aggg'  # Substitua pela senha do app gerada
 
-    mail.send(admin_msg)
+        msg = MIMEMultipart()
+        msg['From'] = smtp_user
+        msg['To'] = smtp_user
+        msg['Subject'] = 'Novo Registro de Usuário'
 
-    # Envia e-mail para o usuário com a URL principal do BLACKVOX
-    user_msg = Message('Acesso ao BLACKVOX',
-                       sender=os.environ.get('MAIL_USERNAME'),
-                       recipients=[email])
-    user_msg.body = f'Olá {username},\n\nObrigado pelo seu interesse no BLACKVOX.\n\nAqui está a URL principal para acesso ao BLACKVOX: https://www.jorgematheus.mus.br/BLACKVOX_PlayPremium.html\n\nAtenciosamente,\nEquipe BLACKVOX'
+        body = f'Nome: {username}\nEmail: {email}\nWhatsApp: {whatsapp}\nMensagem: {mensagem}'
+        msg.attach(MIMEText(body, 'plain'))
 
-    mail.send(user_msg)
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_user, smtp_password)
+        server.sendmail(smtp_user, smtp_user, msg.as_string())
+        server.quit()
+        flash('Registro bem-sucedido! Verifique seu e-mail para mais informações.', 'success')
+    except Exception as e:
+        flash(f'Falha ao enviar o e-mail. Por favor, tente novamente. Erro: {str(e)}', 'danger')
+        app.logger.error(f'Erro ao processar registro: {str(e)}')  # Log de erro detalhado
 
-    return redirect('/')
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
 
 
