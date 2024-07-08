5316d5da68362d93f69d037c56cc9c96a03f87ee
Jorge Luis Aparecido Matheus <jorge@jorgematheus.mus.br>
2024-07-08 01:02:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.environ.get('www.jorgematheus.mus.br@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('dqua lyrj atat aggg')
 
 mail = Mail(app)
 
