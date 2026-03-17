a326a82ac084b5e77173cf22dee23cc60c70de74
Jorge Luis Aparecido Matheus <jorge@jorgematheus.mus.br>
2026-03-16 23:26:15 -0300

    Update contato.js

--- api/contato.js
+++ api/contato.js
@@ -1,1 +1,13 @@
+export default async function handler(req, res) {
 
+  if (req.method !== 'POST') {
+    return res.status(405).send('Método não permitido');
+  }
+
+  const { nome, email, mensagem } = req.body;
+
+  console.log('Novo contato:', nome, email, mensagem);
+
+  return res.status(200).json({ success: true });
+
+}
